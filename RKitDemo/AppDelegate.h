55978fd5980ea3e73cebd64d2ed7ebf938905f4a
上官风雨 <15635395802@163.com>
2017-09-26 10:27:35 +0800

    添加工程

---
+++ RKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RKitDemo
+//
+//  Created by xiaogang.zhang on 2017/9/21.
+//  Copyright © 2017年 Raiing Medical. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RKitDemo
+//
+//  Created by xiaogang.zhang on 2017/9/21.
+//  Copyright © 2017年 Raiing Medical. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
